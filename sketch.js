7b733ae318fb7912fc0e698bfbfb66168c69f51d
yoyete16 <91971870+yoyete16@users.noreply.github.com>
2022-12-13 13:44:47 +0100

    Index

---
+++ sketch.js
@@ -0,0 +1,15 @@
+<head>
+        <script> src="https://cdn.jsdelivr.net/npm/p5@1.4.2/lib/p5.js"></script>
+    
+    </head>
+    <body>
+        <h1>plantilla para P5.JS</h1>
+        <form>
+            <p>¿Cuál es tu fruta favorita?</p>
+            <input type="radio" id="manzana" name="fruta" value="manzana">
+            <label for="manzana">Manzana</label><br>
+            <input type="radio" id="platano" name="fruta" value="platano">
+            <label for="platano">Plátano</label><br>
+            <input type="r) {
+        
+    }
